4dfaf8fa11b9506c0ac6f983ba01fff959fb6a4f
hoaftq <hoaftq@gmail.com>
2023-05-04 23:10:15 +0800

    Adding new word page

---
+++ word-boost-web/pages/add-word.tsx
@@ -0,0 +1,77 @@
+import { Button, Container, FormControl, Stack, TextField, Typography } from "@mui/material";
+import { Controller, useForm } from "react-hook-form"
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import getConfig from "next/config";
+
+interface WordForm {
+    value: string;
+    unit: string;
+    course: string;
+}
+
+export default function AddWord() {
+    const { control, formState: { errors }, handleSubmit, reset } = useForm({
+        defaultValues: {
+            value: '',
+            unit: '',
+            course: ''
+        }
+    });
+
+    const onSubmit = (data: WordForm) => {
+        const { publicRuntimeConfig: { apiUrl } } = getConfig();
+        fetch(`${apiUrl}/add`, {
+            method: "post",
+            body: JSON.stringify(data)
+        })
+            .then(resp => resp.text())
+            .then((wordId: string) => {
+                alert(`Added a new word with id of ${wordId}`);
+                reset();
+            });
+    }
+
+    return (
+        <Container maxWidth="sm">
+            <Typography variant="h5" mb={3}>Add a new word</Typography>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Stack direction={"column"} spacing={2} >
+                    <Controller name="value"
+                        control={control}
+                        rules={{ required: { value: true, message: 'Word is required.' } }}
+                        render={({ field }) => <TextField {...field}
+                            label="Word"
+                            size="small"
+                            error={!!errors?.value}
+                            helperText={errors?.value?.message}
+                        />}
+                    />
+                    <Controller name="unit"
+                        control={control}
+                        rules={{ required: { value: true, message: 'Unit is required.' } }}
+                        render={({ field }) => <TextField {...field}
+                            label="Unit"
+                            size="small"
+                            error={!!errors.unit}
+                            helperText={errors?.unit?.message}
+                        />}
+                    />
+                    <Controller name="course"
+                        control={control}
+                        rules={{ required: { value: true, message: 'Course is required.' } }}
+                        render={({ field }) => <TextField {...field}
+                            label="Course"
+                            size="small"
+                            error={!!errors.course}
+                            helperText={errors?.course?.message}
+                        />}
+                    />
+                    <Button variant="contained"
+                        sx={{ width: 100, alignSelf: "flex-end" }}
+                        startIcon={<AddCircleIcon />}
+                        type="submit">Add</Button>
+                </Stack>
+            </form>
+        </Container>
+    )
+}

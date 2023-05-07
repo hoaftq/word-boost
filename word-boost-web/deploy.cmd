cc8786bbcc65415f1ed439abe4af06c76f61ac9f
hoaftq <hoaftq@gmail.com>
2023-05-08 01:12:21 +0800

    Validated system variables for deployment

--- word-boost-web/deploy.cmd
+++ word-boost-web/deploy.cmd
@@ -1,4 +1,14 @@
-REM API_URL and WORD_BOOST_DISTRIBUTION_ID system variables must be set before running this script
+@echo off
+if "%API_URL%"=="" (
+  echo Please set API_URL with "set API_URL=<your api url>"
+  exit /b
+)
+
+if "%WORD_BOOST_DISTRIBUTION_ID%"=="" (
+  echo Please set WORD_BOOST_DISTRIBUTION_ID with "set WORD_BOOST_DISTRIBUTION_ID=<your cloudfront distribution id>"
+  exit /b
+)
+
 npm run build ^
  && aws s3 sync ./out s3://word-boost ^
  && aws cloudfront create-invalidation --distribution-id %WORD_BOOST_DISTRIBUTION_ID% --path "/*"

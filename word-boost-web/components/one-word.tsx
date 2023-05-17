43edf45da7af0b989adf91ea7120e2dda4c13f76
hoaftq <hoaftq@gmail.com>
2023-05-17 22:46:47 +0800

    Added youtube video support

--- word-boost-web/components/one-word.tsx
+++ word-boost-web/components/one-word.tsx
@@ -1,4 +1,4 @@
-import { Stack, Chip, Button, Accordion as MuiAccordion, AccordionDetails, AccordionSummary, Typography, Tabs, Tab, styled, AccordionProps } from "@mui/material";
+import { Stack, Chip, Button, Accordion as MuiAccordion, AccordionDetails, AccordionSummary, Typography, Tabs, Tab, styled, AccordionProps, IconButton } from "@mui/material";
 import { useEffect, useRef, useState } from "react";
 import { Word } from "./word-list";
 import SkipNextIcon from '@mui/icons-material/SkipNext';
@@ -8,6 +8,7 @@ import { LoadingImage } from "./loading-image";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import AbcIcon from '@mui/icons-material/Abc';
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import ReplayCircleFilledIcon from '@mui/icons-material/ReplayCircleFilled';
 
 export function OneWord({ words, initialImageVisible }: { words: Word[], initialImageVisible: boolean }) {
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -125,6 +126,38 @@ const Accordion = styled((props: AccordionProps) => (
     },
 }));
 
+function EmbbebedYoutube({ videoUrl }: { videoUrl: string }) {
+    const iframeRef = useRef(null);
+    const autoplayQueryString = "autoplay=1";
+
+    const handleReplayClick = () => {
+        if (videoUrl.indexOf(autoplayQueryString) < 0) {
+            if (videoUrl.indexOf(".") > 0) {
+                videoUrl = `${videoUrl}&${autoplayQueryString}`;
+            } else {
+                videoUrl = `${videoUrl}?${autoplayQueryString}`
+            }
+        }
+
+        (iframeRef.current as any).src = videoUrl;
+    }
+
+    return (
+        <>
+            <iframe ref={iframeRef} width="800"
+                height="450"
+                src={videoUrl}
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen"
+                allowFullScreen></iframe>
+            <IconButton onClick={handleReplayClick}>
+                <ReplayCircleFilledIcon />
+            </IconButton>
+        </>
+    )
+}
+
 function SentenceCard({ word }: { word: Word }) {
     const [expandedIndex, setExpandedIndex] = useState(-1);
 
@@ -138,17 +171,26 @@ function SentenceCard({ word }: { word: Word }) {
         return styledSentence;
     }
 
+    const isYoutubeLink = (medialUrl: string) => medialUrl.startsWith("https://youtu.be") || medialUrl.startsWith("https://www.youtube");
+
     return (
         <div style={{ width: "100%" }}>
             {word.sentences.map((s, i) => (
                 <Accordion key={s.value} expanded={expandedIndex == i} onChange={(_, isExpanded) => handleChange(i, isExpanded)}>
                     <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                        <Typography variant="h5" component="div" color="primary" fontWeight="bold" dangerouslySetInnerHTML={{ __html: highlightWord(s.value) }}></Typography>
+                        <Typography variant="h5"
+                            component="div"
+                            color={expandedIndex === i || expandedIndex === -1 ? "primary" : "gray"}
+                            fontWeight="bold"
+                            dangerouslySetInnerHTML={{ __html: highlightWord(s.value) }} />
                     </AccordionSummary>
                     <AccordionDetails>
-                        <div style={{ width: "100%", height: 300 }}>
-                            <LoadingImage imageUrl={s.mediaUrl} />
-                        </div>
+                        {isYoutubeLink(s.mediaUrl) ?
+                            <EmbbebedYoutube videoUrl={s.mediaUrl} /> :
+                            <div style={{ width: "100%", height: 300 }}>
+                                <LoadingImage imageUrl={s.mediaUrl} />
+                            </div>
+                        }
                     </AccordionDetails>
                 </Accordion>
             ))}

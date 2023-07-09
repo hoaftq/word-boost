f718b257bd9abfd95348c02b5291bcf314e54603
hoaftq <hoaftq@gmail.com>
2023-07-09 23:22:20 +0800

    Added an api to get units by courses

---
+++ word-boost-api/WordBoostFunctions/src/main/java/wordboost/UnitCourseDto.java
@@ -0,0 +1,13 @@
+package wordboost;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@AllArgsConstructor
+@EqualsAndHashCode
+public class UnitCourseDto {
+    private String unit;
+    private String course;
+}

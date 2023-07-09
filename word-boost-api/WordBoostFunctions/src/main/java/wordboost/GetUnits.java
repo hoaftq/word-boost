5eefba97e7ebc9cdc2b13f54a8c110843c16c5cd
hoaftq <hoaftq@gmail.com>
2023-07-09 23:40:53 +0800

    Refactoring GetUnits

--- word-boost-api/WordBoostFunctions/src/main/java/wordboost/GetUnits.java
+++ word-boost-api/WordBoostFunctions/src/main/java/wordboost/GetUnits.java
@@ -1,38 +1,29 @@
 package wordboost;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.model.ScanRequest;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 
-import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
-public class GetUnits implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
-
-    private final String wordsTableName = System.getenv("WORDS_TABLE");
-
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
-    private final AmazonDynamoDB dynamoDB = DynamoDBUtil.GetAmazonDynamoDB();
+public class GetUnits extends FunctionBase implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
     @SneakyThrows
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
-        var result = dynamoDB.scan(new ScanRequest().withTableName(wordsTableName));
-        var words = result.getItems().stream()
+        var scanRequest = new ScanRequest().withTableName(wordsTableName)
+                .withProjectionExpression("#unit")
+                .withExpressionAttributeNames(Map.of("#unit", "unit"));
+        var units = dynamoDB.scan(scanRequest).getItems()
+                .stream()
                 .map(i -> i.get("unit").getS())
                 .distinct()
                 .sorted()
                 .collect(Collectors.toList());
-        return new APIGatewayProxyResponseEvent()
-                .withHeaders(new HashMap<>() {{
-                    put("Access-Control-Allow-Origin", "*");
-                }})
-                .withBody(objectMapper.writeValueAsString(words));
+        return createResponse(units);
     }
 }

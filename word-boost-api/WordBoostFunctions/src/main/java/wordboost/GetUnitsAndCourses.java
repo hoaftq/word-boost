2098a5ab27a99ef93f35cce9be1ba960500d69e1
hoaftq <hoaftq@gmail.com>
2023-07-11 11:32:36 +0800

    Api to get units and courses at the same time to reduce api calls

--- word-boost-api/WordBoostFunctions/src/main/java/wordboost/GetUnitsAndCourses.java
+++ word-boost-api/WordBoostFunctions/src/main/java/wordboost/GetUnitsAndCourses.java
@@ -6,18 +6,21 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import wordboost.common.FunctionBase;
+import wordboost.dtos.UnitCourseDto;
 
+import java.util.Map;
 import java.util.stream.Collectors;
 
-public class GetCourses extends FunctionBase implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+public class GetUnitsAndCourses extends FunctionBase implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent, Context context) {
         var scanRequest = new ScanRequest()
                 .withTableName(wordsTableName)
-                .withProjectionExpression("course");
+                .withProjectionExpression("course, #unit")
+                .withExpressionAttributeNames(Map.of("#unit", "unit"));
         var courses = amazonDynamoDB.scan(scanRequest)
                 .getItems()
-                .stream().map(i -> i.get("course").getS())
+                .stream().map(i -> new UnitCourseDto(i.get("unit").getS(), i.get("course").getS()))
                 .distinct()
                 .sorted()
                 .collect(Collectors.toList());

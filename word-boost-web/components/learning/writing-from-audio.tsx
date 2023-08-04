011f410b95aff996cfa514236e6ab585188dc9ee
hoaftq <hoaftq@gmail.com>
2023-08-05 01:50:14 +0800

    Fixed the bug: last sentence never ends and some improvement

--- word-boost-web/components/learning/writing-from-audio.tsx
+++ word-boost-web/components/learning/writing-from-audio.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { Word } from "../main";
 import { CombinedSentence } from "../testing/fill-blank-test";
 import { combineSentences } from "@wb/utils/utils";
@@ -14,13 +14,8 @@ const MaxDelayTime = 15000;
 const AverageDisplayTimeForAWord = 2500;
 
 export function WritingFromAudio({ words }: { words: Word[] }) {
-    const [combinedSentences, setCombinedSentences] = useState<CombinedSentence[] | null>(null);
     const [sentenceIndex, setSentenceIndex] = useState(0);
-
-    // TODO move this out of all the components
-    if (!combinedSentences) {
-        setCombinedSentences(combineSentences(words));
-    }
+    const combinedSentences = useMemo(() => combineSentences(words), [words]);
 
     function handleFinish(): void {
         if (sentenceIndex < combinedSentences!.length - 1) {
@@ -35,24 +30,30 @@ export function WritingFromAudio({ words }: { words: Word[] }) {
 function WritingSenntenceFromAudio({ combinedSentence, onFinish }: { combinedSentence: CombinedSentence, onFinish: () => void }) {
     const [repeat, setRepeat] = useState(0);
     const [sentenceVisible, setSentenceVisible] = useState(false);
+    const [prevCombinedSentence, setPrevCombinedSentence] = useState<CombinedSentence | null>(null);
 
     const wordCount = combinedSentence.sentence.value.split(" ").length;
     const repeatCount = wordCount * 2;
     const intervalRate = (EndRate - StartRate) / repeatCount;
 
-    function handleFinish(): void {
-        if (repeat >= repeatCount - 1) {
-            setRepeat(0);
-            setSentenceVisible(false);
+    const rate = repeat === 0 ? EndRate : StartRate + intervalRate * (repeat - 1);
 
+    if (prevCombinedSentence !== combinedSentence) {
+        setRepeat(0);
+        setSentenceVisible(false);
+        setPrevCombinedSentence(combinedSentence);
+    }
+
+    function handleFinish(): void {
+        if (repeat >= repeatCount) {
             setTimeout(() => {
                 onFinish();
-            }, 5000);
+            }, 10000);
             return;
         }
 
-        setTimeout(() => {
-            if (repeat % 3 === 2) {
+        window.setTimeout(() => {
+            if (repeat % 3 === 1) {
                 setSentenceVisible(true);
                 setTimeout(() => {
                     setSentenceVisible(false);
@@ -68,11 +69,11 @@ function WritingSenntenceFromAudio({ combinedSentence, onFinish }: { combinedSen
                 Listen and write
             </Typography>
             <AudioPlayer videoUrl={combinedSentence.sentence.mediaUrl}
-                rate={StartRate + intervalRate * repeat}
+                rate={rate}
                 repeat={repeat}
                 onFinish={handleFinish} />
             <Typography variant="h6" fontWeight="bold">
-                Repeat times: {repeat + 1} / {repeatCount}
+                Repeat times: {repeat} / {repeatCount}
             </Typography>
         </div>
         <Typography fontSize={55} color="primary">

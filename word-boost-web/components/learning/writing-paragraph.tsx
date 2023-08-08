abc0e247f758984b770c30f5197de5983363a3af
hoaftq <hoaftq@gmail.com>
2023-08-08 18:08:34 +0800

    Adding audio to writing paragraphs

--- word-boost-web/components/learning/writing-paragraph.tsx
+++ word-boost-web/components/learning/writing-paragraph.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useMemo, useRef } from "react";
+import { useEffect, useMemo, useRef, useState, UIEvent } from "react";
 import { Word } from "../main";
 import { combineSentences } from "@wb/utils/utils";
-import { TimeForALetterInSeconds, WritingSentenceWithOrigin } from "./writing-sentence";
+import { WritingSentenceWithOrigin } from "./writing-sentence";
+import { AudioPlayer } from "../common/sentence-audio-player";
 
 type WritingParagraphProps = {
     words: Word[];
@@ -10,39 +11,102 @@ type WritingParagraphProps = {
 export function WritingParagraph({ words, speed }: WritingParagraphProps) {
     const combinedSentences = useMemo(() => combineSentences(words), [words]);
     const containerRef = useRef<HTMLDivElement>(null);
-    const countRef = useRef(0);
+
+    const [playerPosition, setPlayerPosition] = useState({ top: 0, left: 0 });
+
+    const sentencesRef = useRef<Array<HTMLDivElement | null>>([]);
+    const [focusIndex, setFocusIndex] = useState(0);
+
+    const scannerPositionRef = useRef(0);
+
+    function handleScroll(event: UIEvent<HTMLDivElement>): void {
+        // Make sure the sanner is in the middle for manually scrolling
+        scannerPositionRef.current = containerRef.current!.clientHeight / 2;
+        setFocusSentence();
+    }
 
     useEffect(() => {
-        if (!combinedSentences || combinedSentences.length === 0) {
-            return;
+        function moveScannerDown(pos: number, done?: () => void) {
+            const timerId = window.setInterval(() => {
+                scannerPositionRef.current += speed;
+                setFocusSentence();
+
+                if (scannerPositionRef.current >= pos) {
+                    window.clearInterval(timerId);
+                    done?.();
+                }
+            }, 1000);
+
+            return timerId;
         }
 
-        const delayForFirstSentence = TimeForALetterInSeconds * combinedSentences[0].sentence.value.length * 2 / 3;
-        const timerId = setInterval(() => {
-            if (countRef.current < delayForFirstSentence) {
-                countRef.current++;
-                return;
-            }
+        function scrollDown(done: () => void) {
+            const timerId = window.setInterval(() => {
+                const container = containerRef.current!;
+                container.scrollBy(0, speed);
 
-            const container = containerRef.current!;
+                if (Math.abs(container.scrollHeight - container.scrollTop - container.clientHeight) < 1) {
+                    window.clearInterval(timerId);
+                    done();
+                }
+            }, 1000);
 
-            container.scrollBy(0, speed);
-            if (Math.abs(container.scrollHeight - container.scrollTop - container.clientHeight) < 1) {
-                clearInterval(timerId);
-            }
-        }, 1000);
+            return timerId;
+        }
+
+        let timerId1: number, timerId2: number, timerId3: number;
+        timerId1 = moveScannerDown(containerRef.current!.clientHeight / 2, () => {
+            timerId2 = scrollDown(() => {
+                timerId3 = moveScannerDown(containerRef.current!.clientHeight);
+            });
+        })
 
         return () => {
-            clearInterval(timerId);
-        };
-    }, [combinedSentences, speed])
+            clearInterval(timerId1);
+            clearInterval(timerId2);
+            clearInterval(timerId3);
+        }
+    }, [speed]);
+
+    // Put the player to the desire position
+    useEffect(() => {
+        const container = containerRef.current!;
+        setPlayerPosition({
+            top: container.offsetTop,
+            left: container.offsetLeft + container.clientWidth - 50,
+        });
+    }, []);
+
+    function setFocusSentence() {
+        const container = containerRef.current!;
+        for (let i = sentencesRef.current.length - 1; i >= 0; i--) {
+            if (sentencesRef.current[i]!.offsetTop < container.scrollTop + container.offsetTop + scannerPositionRef.current) {
+                setFocusIndex(i);
+                break;
+            }
+        }
+    }
 
     return combinedSentences
         && <div ref={containerRef}
             style={{
                 height: "calc(100vh - 80px)",
                 overflowY: "auto"
+            }} onScroll={handleScroll}>
+            {combinedSentences.map((cs, i) => <>
+                <WritingSentenceWithOrigin key={cs.sentence.value}
+                    ref={(node) => { sentencesRef.current[i] = node }}
+                    sentence={cs.sentence.value} focused={focusIndex === i} />
+            </>)}
+            <div style={{
+                position: "fixed",
+                top: playerPosition.top,
+                left: playerPosition.left,
+                width: 50,
+                height: 40,
+                backgroundColor: "red"
             }}>
-            {combinedSentences.map((cs) => <WritingSentenceWithOrigin key={cs.sentence.value} sentence={cs.sentence.value} />)}
+                <AudioPlayer videoUrl={combinedSentences[focusIndex].sentence.mediaUrl} />
+            </div>
         </div>
 }
--- word-boost-web/components/learning/writing-sentence.tsx
+++ word-boost-web/components/learning/writing-sentence.tsx
@@ -1,20 +1,33 @@
 import { Typography } from "@mui/material";
 import { TracingLetter } from "./tracing-letter";
+import { ForwardedRef, forwardRef } from "react";
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
 export const TimeForALetterInSeconds = 6;
 
 type WritingSentenceProps = {
     sentence: string;
+    focused?: boolean;
 }
 
-export function WritingSentenceWithOrigin({ sentence }: WritingSentenceProps) {
-    return <>
-        <Typography fontSize={50}>
+export const WritingSentenceWithOrigin = forwardRef(function WritingSentenceWithOrigin(
+    { sentence, focused }: WritingSentenceProps,
+    ref: ForwardedRef<HTMLDivElement>
+) {
+    return <div ref={ref}>
+        {focused && <PlayArrowIcon htmlColor="white"
+            sx={{
+                backgroundColor: "red",
+                width: 50,
+                height: 40,
+                marginRight: 2
+            }} />}
+        <Typography fontSize={50} component={"span"} color={focused ? "error" : "black"}>
             {sentence}
         </Typography>
         <WritingSentence sentence={sentence} />
-    </>
-};
+    </div>
+});
 
 export function WritingSentence({ sentence }: WritingSentenceProps) {
     return (

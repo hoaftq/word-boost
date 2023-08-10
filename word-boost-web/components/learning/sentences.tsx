98704f852c726cbb4160c670215c7b53c9d408dc
hoaftq <hoaftq@gmail.com>
2023-08-10 22:33:36 +0800

    Add audio player to reading sentences

--- word-boost-web/components/learning/sentences.tsx
+++ word-boost-web/components/learning/sentences.tsx
@@ -1,27 +1,44 @@
-import { Paper } from "@mui/material";
+import { Paper, Stack } from "@mui/material";
 import { Word } from "../main";
 import { CombinedSentenceTypography } from "./sentence-typography";
 import { combineSentences } from "@wb/utils/utils";
+import { AudioPlayer } from "../common/sentence-audio-player";
+import { useMemo, useState } from "react";
 
 export function Sentences({ words }: { words: Word[] }) {
-    const combinedSentences = combineSentences(words);
+    const combinedSentences = useMemo(() => combineSentences(words), [words]);
+    const [focusIndex, setFocusIndex] = useState(0);
+
+    function handleClick(index: number): void {
+        setFocusIndex(index);
+    }
 
     return (
         <Paper elevation={3}
             sx={{
                 padding: 1
             }}>
-            {combinedSentences.map((s, i) => <CombinedSentenceTypography key={s.sentence.value}
-                combinedSentence={s}
-                variant="h4"
-                component="div"
-                color="primary"
-                fontSize={50}
-                fontWeight="bold"
-                sx={{
-                    marginX: 1,
-                    marginY: 3
-                }} />)}
+            {combinedSentences.map((s, i) => (
+                <Stack key={s.sentence.value}
+                    direction={"row"}
+                    sx={{
+                        alignItems: "center",
+                        gap: 2,
+                        marginX: 1,
+                        marginY: 2
+                    }}>
+                    <CombinedSentenceTypography
+                        combinedSentence={s}
+                        variant="h4"
+                        component="div"
+                        color="primary"
+                        fontSize={50}
+                        fontWeight="bold"
+                        onClick={() => handleClick(i)}
+                    />
+                    {focusIndex === i && <AudioPlayer videoUrl={s.sentence.mediaUrl} autoplay={false} />}
+                </Stack>)
+            )}
         </Paper>
     );
 }
